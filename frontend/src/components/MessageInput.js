bfb928a71fd260b114044d5327f1ae1dd92af533
Hanna Skoromna <hanna.skoromna@gmail.com>
2019-09-30 23:01:27 +0200

    misc - code refactoring

--- frontend/src/components/MessageInput.js
+++ frontend/src/components/MessageInput.js
@@ -1,45 +1,44 @@
-import React, { Component } from 'react'
-import { Col, Form } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Col, Form, Row } from 'react-bootstrap'
 import { StyledButton, StyledInput } from './Styles'
-import Row from 'react-bootstrap/Row'
 
-class ChatInput extends Component {
-  state = {
-    message: ''
+const ChatInput = ({ onTyping, onSubmitMessage }) => {
+  const [message, setMessage] = useState('')
+
+  const onChange = e => {
+    setMessage(e.target.value)
+    onTyping()
   }
-  onChange = e => {
-    this.setState({ message: e.target.value })
-    this.props.onTyping()
-  }
-  render() {
-    return (
-      <Form
-        action=""
-        onSubmit={e => {
-          e.preventDefault()
-          this.props.onSubmitMessage(this.state.message)
-          this.setState({ message: '' })
-        }}
-      >
-        <Form.Group as={Row} controlId="messageInput">
-          <Form.Label column sm={2}>
-            Message:
-          </Form.Label>
-          <Col sm={4}>
-            <StyledInput
-              type="text"
-              placeholder={'Enter message...'}
-              value={this.state.message}
-              onChange={e => this.onChange(e)}
-            />
-          </Col>
-          <Col sm={2}>
-            <StyledButton type="submit">Send</StyledButton>
-          </Col>
-        </Form.Group>
-      </Form>
-    )
-  }
+
+  return (
+    <Form
+      action=""
+      onSubmit={e => {
+        e.preventDefault()
+        onSubmitMessage(message)
+        setMessage('')
+      }}
+    >
+      <Form.Group as={Row} controlId="messageInput">
+        <Form.Label column sm={2}>
+          Message:
+        </Form.Label>
+        <Col sm={4}>
+          <StyledInput
+            type="text"
+            placeholder={'Enter message...'}
+            value={message}
+            onChange={onChange}
+          />
+        </Col>
+        <Col sm={2}>
+          <StyledButton type="submit">Send</StyledButton>
+        </Col>
+      </Form.Group>
+    </Form>
+  )
 }
+
+ChatInput.displayName = 'ChatInput'
 
 export default ChatInput
